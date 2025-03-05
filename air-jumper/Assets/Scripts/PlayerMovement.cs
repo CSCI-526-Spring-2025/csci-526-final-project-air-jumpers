b63dd5f5b436a94a1b730efc5b893afa7ebb04ce
Daniel Tang <danieltangzs1999@gmail.com>
2025-03-05 00:10:47 -0800

    Setup hooks in game to send the data to google form
    Setup hooks for _newPlatformCount and _levelElapsedTime to keep track of new platform created by used and how long time does user spend to complete level 1

--- air-jumper/Assets/Scripts/PlayerMovement.cs
+++ air-jumper/Assets/Scripts/PlayerMovement.cs
@@ -9,17 +9,28 @@ public class PlayerMovement : MonoBehaviour
     public TextMeshProUGUI platformCounterText;
     private Rigidbody2D rb;
     private int platformCount = 1;
+
+    // platformCount tracks how many platforms are left to be created
+    // private int platformCount = 10; 
+
+    // plaformUsed tracks how many plaforms has been created;
+    private int platformCreated = 0;
     private bool isGrounded = false;
     private bool isOnPlatform = false;
     private bool facingRight = true;
 
     private int jumpTimes = 1;
     private Vector3 startPosition;
+
+    private float startTime; // Stores the game start time
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         startPosition = transform.position;
         UpdatePlatformCounter();
+
+        StartTime(); // Start the game timer
     }
 
     void Update()
@@ -79,7 +90,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y - 1.2f, 0);
         Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
         platformCount--;
+        platformCreated++;
         UpdatePlatformCounter();
+
         if (platformCount == 0)
         {
             FindObjectOfType<GameOverManager>().StartGameOverTimer();
@@ -193,5 +206,34 @@ public void RespawnPlayer()
         transform.position = startPosition; //No checkpoint reached yet
     }
 }
+
+
+    /// <summary>
+    /// Gets the total number of platforms created by the player.
+    /// </summary>
+    /// <returns>The number of platforms the player has created.</returns>
+    public int getPlatformCreated()
+    {
+        // Debug.Log(platformCreated); // Uncomment for debugging if needed
+        return platformCreated;
+    }
+
+    /// <summary>
+    /// Starts the timer by recording the current game time.
+    /// This should be called when the game begins or when timing needs to be reset.
+    /// </summary>
+    void StartTime()
+    {
+        startTime = Time.time; // Store the current time as the start time
+    }
+
+    /// <summary>
+    /// Retrieves the elapsed time since the timer started.
+    /// </summary>
+    /// <returns>The total elapsed time in seconds.</returns>
+    public float getElapsedTime()
+    {
+        return Time.time - startTime; // Calculate elapsed time by subtracting startTime from the current time
+    }
 
 }
--- air-jumper/Assets/Scripts/SendToGoogle.cs
+++ air-jumper/Assets/Scripts/SendToGoogle.cs
@@ -4,29 +4,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-
+/// <summary>
+/// Handles sending player analytics data to a Google Form.
+/// </summary>
 public class SendToGoogle : MonoBehaviour
 {
     [SerializeField] private string URL;
 
     private long _sessionID;
     private int _newPlatformCount;
-    private int _reachedLevel;
+    private int _reachedCheckpoints;
     // Start is called before the first frame update
+
+    private int _collectiblesCount;
+    private int _enemyDefeated;
+
+    private float _levelElapsedTime;
+
+    PlayerMovement playerMovement;
 
     private void Awake()
     {
         _sessionID = DateTime.Now.Ticks;
 
-        Send();
+        // Send();
     }
 
-    private IEnumerator Post(string sessionID, string newPlatformCount, string reachedLevel)
+    /// <summary>
+    /// Sends collected player data to a Google Form using an HTTP POST request.
+    /// </summary>
+    /// <param name="sessionID">Unique session identifier</param>
+    /// <param name="newPlatformCount">Number of platforms created</param>
+    /// <param name="reachedLevel">Highest level reached</param>
+    /// /// <param name="levelElapsedTime">Time to finish one level</param>
+    /// <returns>Coroutine that sends data asynchronously</returns>
+    private IEnumerator Post(string sessionID, string newPlatformCount, string reachedCheckedPoints, string levelElapsedTime)
     {
         WWWForm form = new WWWForm();
         form.AddField("entry.595435075", sessionID);
         form.AddField("entry.220358108", newPlatformCount);
-        form.AddField("entry.1107099891", reachedLevel);
+        form.AddField("entry.1107099891", reachedCheckedPoints);
+        form.AddField("entry.2002507409", levelElapsedTime);
 
         // Debug.Log(sessionID);
 
@@ -45,23 +63,35 @@ public class SendToGoogle : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Collects player data and starts the upload process.
+    /// </summary>
     public void Send()
     {
-        _newPlatformCount = 10;
-        _reachedLevel = 2;
+        _newPlatformCount = playerMovement.getPlatformCreated();
+        _levelElapsedTime = playerMovement.getElapsedTime();
+        _reachedCheckpoints = 0;
 
-        StartCoroutine(Post(_sessionID.ToString(), _newPlatformCount.ToString(), _reachedLevel.ToString()));
+        StartCoroutine(Post(_sessionID.ToString(), _newPlatformCount.ToString(), _reachedCheckpoints.ToString(), _levelElapsedTime.ToString("F2")));
     }
 
+    /// <summary>
+    /// Called when the application is about to quit.
+    /// This ensures that final analytics data is sent before the game closes.
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+        // Debug.Log(playerMovement.getPlatformCount());
+        Debug.Log("Game is quitting. Sending analytics...");
+        Send();
+    }
 
+    /// <summary>
+    /// Initializes references when the game starts.
+    /// </summary>
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Find the PlayerMovement script in the scene
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
 }
