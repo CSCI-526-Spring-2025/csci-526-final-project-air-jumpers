76cba7758bcc9ad5d47e07446e758ef55f4d8279
Haonan Su <kevinsu99@outlook.com>
2025-03-08 20:41:07 -0800

    build: alpha stage

--- air-jumper/Assets/Editor/BuildScript.cs
+++ air-jumper/Assets/Editor/BuildScript.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 
 public class BuildScript
 {
-    public static string tagPrefix = "alpha-progress";
+    public static string tagPrefix = "alpha";
 
     private static string repoRoot = Directory.GetParent(Directory.GetParent(Application.dataPath).FullName).FullName;
 
