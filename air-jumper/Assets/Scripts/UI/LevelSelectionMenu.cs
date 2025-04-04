15a5f4b1deca1c734cfd5f658de5212012274972
Devesh Bhatla <deveshbhatla952@gmail.com>
2025-04-03 18:28:11 -0700

    added menu switch script

---
+++ air-jumper/Assets/Scripts/UI/LevelSelectionMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionMenu : MonoBehaviour
+{
+    public GameObject levelSelectionPanel; 
+
+    private void Start()
+    {
+        levelSelectionPanel.SetActive(false); 
+    }
+
+    public void OpenLevelSelection()
+    {
+        levelSelectionPanel.SetActive(true);
+    }
+
+    public void CloseLevelSelection()
+    {
+        levelSelectionPanel.SetActive(false);
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
